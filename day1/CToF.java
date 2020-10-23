3ce289f00a7ae17072c8936ee541baecd5a7f0b5
imvaishu <vaishnaviyadav4558@gmail.com>
2020-10-23 12:03:50 +0530

    extracted function from main from CToF function

--- day1/CToF.java
+++ day1/CToF.java
@@ -1,13 +1,16 @@
 public class CToF {
+  public static double getCToF(double centigradeTemp) {
+    return  1.8 * centigradeTemp + 32;
+  }
+
   public static void main(String[] args) {
     if(args.length < 1){
       System.out.println("Please give valid input");
       return;
     } 
-    
+
       double centigradeTemp = Integer.parseInt(args[0]);
-      double fahrenheitTemp = 1.8 * centigradeTemp + 32;
-      
-      System.out.println("centigrade to fahrenheit of " + args[0] + " is " + fahrenheitTemp);
+     
+      System.out.println("centigrade to fahrenheit of " + args[0] + " is " + getCToF(centigradeTemp));
   }
 }
