1741ab733adea7c582280b75904a037f882379d7
imvaishu <vaishnaviyadav4558@gmail.com>
2020-10-23 11:22:10 +0530

    modified extracted simple interest method from main in simple interest function

--- day1/SimpleInterest.java
+++ day1/SimpleInterest.java
@@ -1,17 +1,20 @@
 public class SimpleInterest {
+  public static double getSI(int principle,int rateOfInterest,int period) {
+    return (principal * rateOfInterest * period) / 100 ;
+  }
+
   public static void main(String[] args) {
-    
     if(args.length < 3){
       System.out.println("Please give valid input");
       return;
     } 
 
       int principal = Integer.parseInt(args[0]);
-      double rateOfInterest = Integer.parseInt(args[1]);
+      int rateOfInterest = Integer.parseInt(args[1]);
       int period = Integer.parseInt(args[2]);
 
-      double simpleInterest = (principal * rateOfInterest * period) / 100;
+      double simpleInterest = getSI(principal, rateOfInterest, period);
 
-      System.out.println(simpleInterest);
+      System.out.println("Simple interest is " + simpleInterest);
   }
 }
