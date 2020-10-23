9dad013a704df2aa0b96f4ed2b8f84f46db7fa9a
imvaishu <vaishnaviyadav4558@gmail.com>
2020-10-23 12:07:28 +0530

    extracted function from main from Fibonacci function

--- day1/Fibonacci.java
+++ day1/Fibonacci.java
@@ -1,15 +1,8 @@
 public class Fibonacci{
-  public static void main(String[] args){
+  public static void displayFibonacci(int limit) {
     int firstTerm = 0;
     int secondTerm = 1;
     int sum;
-    
-    if(args.length < 1){
-      System.out.println("Please give valid limit");
-      return;
-    } 
-
-    int limit = Integer.parseInt(args[0]);
 
     for(int i = 1; i <= limit ; i++ ){
       System.out.println(firstTerm);
@@ -17,5 +10,15 @@ public class Fibonacci{
       firstTerm = secondTerm;
       secondTerm = sum;
     }
+  }
+
+  public static void main(String[] args){
+    if(args.length < 1){
+      System.out.println("Please give valid input");
+      return;
+    } 
+   
+    int limit = Integer.parseInt(args[0]);
+    displayFibonacci(limit);
   }
 }
