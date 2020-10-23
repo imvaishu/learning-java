293135244956717880b3597f872c6183d893a5ea
imvaishu <vaishnaviyadav4558@gmail.com>
2020-10-23 11:58:47 +0530

    extracted function from main from Compoundinterest function

--- day1/CompoundInterest.java
+++ day1/CompoundInterest.java
@@ -1,16 +1,20 @@
 public class CompoundInterest {
+  public static double getCI(int principal, double rateOfInterest, int period) {
+    double compoundInterest = principal * Math.pow((1 + rateOfInterest / 100 ), period);
+    
+    return compoundInterest - principal;
+  }
+
   public static void main(String[] args) {
     if(args.length < 3){
       System.out.println("Please give valid input");
       return;
     } 
-    
+
       int principal = Integer.parseInt(args[0]);
       double rateOfInterest = Integer.parseInt(args[1]);
       int period = Integer.parseInt(args[2]);
 
-      double compoundInterest = principal * Math.pow((1 + rateOfInterest/100 ),period);
-      
-      System.out.println(compoundInterest - principal);
+      System.out.println(getCI(principal, rateOfInterest, period));
   }
 }
