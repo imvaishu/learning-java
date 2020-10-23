11f4666ee2a0b150f98603acdfd5f59a76109cf5
imvaishu <vaishnaviyadav4558@gmail.com>
2020-10-23 12:02:15 +0530

    extracted function from main from FToC function

--- day1/FToC.java
+++ day1/FToC.java
@@ -1,13 +1,16 @@
 public class FToC {
+  public static double getFToC(double fahrenheitTemp) {
+    return (fahrenheitTemp - 32) * 5 / 9;
+  }
+
   public static void main(String[] args) {
     if(args.length < 1){
       System.out.println("Please give valid input");
       return;
     }
-     
+
       double fahrenheitTemp = Integer.parseInt(args[0]);
-      double centigradeTemp = (fahrenheitTemp - 32) * 5/9;
       
-      System.out.println("fahrenheit to centigrade of " + args[0] + " is " + centigradeTemp);
+      System.out.println("fahrenheit to centigrade of " + args[0] + " is " + getFToC(fahrenheitTemp));
   }
 }
