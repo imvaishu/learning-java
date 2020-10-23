11d48adf5dfc0fa1e95b2e23251a047420cf040f
imvaishu <vaishnaviyadav4558@gmail.com>
2020-10-23 11:51:19 +0530

    extracted function from main from GCD function

--- day1/GCD.java
+++ day1/GCD.java
@@ -1,6 +1,19 @@
 public class GCD {
+  public static int getGcd(int num1, int num2) {
+    int gcd = num1;
+    num1 = Math.max(num1, num2);
+    num2 = Math.min(gcd, num2);
+
+    while(num2 != 0){
+      gcd = num2;
+      num2 = num1 % num2;
+      num1 = gcd;
+    }
+    return gcd;
+  }
+
+
   public static void main(String[] args) {
-    
     if(args.length < 2){
       System.out.println("Please give valid input");
       return;
@@ -8,17 +21,7 @@ public class GCD {
 
       int num1 = Integer.parseInt(args[0]);
       int num2 = Integer.parseInt(args[1]);
-      int gcd = num1;
-     
-      int greatest = Math.max(num1, num2);
-      num1 = greatest;
-      num2 = gcd;
 
-      while(num2 != 0){
-        gcd = num2;
-        num2 = num1 % num2;
-        num1 = gcd;
-      }
-      System.out.println("GCD is " + gcd);
+      System.out.println("GCD is " + getGcd(num1, num2));
   }
 }
