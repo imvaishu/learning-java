37cfcb84d57d17e002e06c0f27173682e356743f
imvaishu <vaishnaviyadav4558@gmail.com>
2020-10-24 12:26:40 +0530

    Added repeat function

---
+++ array/Repeat.java
@@ -0,0 +1,22 @@
+public class Repeat {
+
+  public static void printArray(int[] array) {
+    for (int element : array) {
+      System.out.println(element);
+    }
+  }
+
+  public static int[] range(int numOfTimes, int term) {
+    int[] series = new int[numOfTimes];
+
+    for (int index = 0; index < numOfTimes; index++) {
+      array[index] = term;
+    }
+
+    return series;
+  }
+
+  public static void main(String[] args) {
+    printArray(range(5, 2));
+  }
+}
